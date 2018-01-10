17284b236f7624b5102e542a1cf17c539bd16908
gbatra17 <gbatra17@gmail.com>
2018-01-10 00:16:24 -0600

    Finished reverseArray

--- src/recursion.js
+++ src/recursion.js
@@ -190,7 +190,13 @@ var createArray = function(str) {
 };
 
 // 17. Reverse the order of an array
-var reverseArr = function(array) {};
+var reverseArr = function(array) {
+  if(array.length === 0){
+    return [];
+  }
+
+  return reverseArr(array.slice(1)).concat(array[0]);
+};
 
 // 18. Create a new array with a given value and length.
 // buildList(0,5) // [0,0,0,0,0]
